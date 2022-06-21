84316baa43e4ad677031a2489bba81d99a59802a
Patrick <prs100791@gmail.com>
2022-06-21 01:06:01 -0500

    add startup log

--- src/main/java/com/valhalla/DataLoader.java
+++ src/main/java/com/valhalla/DataLoader.java
@@ -2,16 +2,28 @@ package com.valhalla;
 
 import com.valhalla.clients.DiscordClient;
 
+import java.lang.invoke.MethodHandles;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.micronaut.context.event.ApplicationEventListener;
 import io.micronaut.context.event.StartupEvent;
+import io.micronaut.runtime.server.EmbeddedServer;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 
 @Singleton
 public class DataLoader implements ApplicationEventListener<StartupEvent> {
+	private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup()
+		.lookupClass());
 
 	@Inject
 	private DiscordClient discordClient;
+
+	public DataLoader(final EmbeddedServer embeddedServer) {
+		LOG.info("Listening on {}:{}", embeddedServer.getHost(), embeddedServer.getPort());
+	}
 
 	@Override
 	public void onApplicationEvent(final StartupEvent event) {
